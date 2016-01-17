fc9ecdce25c01ccb7ce3d219188bb688cb54b275
Niristotle Okram <niristotle.okram@ad.infosys.com>
2016-01-17 13:38:09 -0600

    initial commit

---
+++ cookbooks/wrapper-iis/metadata.rb
@@ -0,0 +1,10 @@
+name             'wrapper-iis'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures wrapper-iis'
+long_description 'Installs/Configures wrapper-iis'
+
+version          '0.1.0'
+
+depends 'iis'
---
+++ cookbooks/wrapper-iis/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: wrapper-iis
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'iis'
